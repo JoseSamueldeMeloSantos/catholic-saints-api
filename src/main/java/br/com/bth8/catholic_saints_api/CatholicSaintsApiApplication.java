0ff71d1cb4b4f8274022b360fde64b134b08fae0
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-10-26 18:15:54 -0300

    first commit

---
+++ src/main/java/br/com/bth8/catholic_saints_api/CatholicSaintsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bth8.catholic_saints_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatholicSaintsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatholicSaintsApiApplication.class, args);
+	}
+
+}
