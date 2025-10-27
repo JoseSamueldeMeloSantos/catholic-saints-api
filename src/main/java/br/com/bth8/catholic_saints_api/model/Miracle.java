9c5d97dcd84d4faf839b8e16e1e0298e71ee866e
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-10-27 08:58:56 -0300

    miracle + saint

--- src/main/java/br/com/bth8/catholic_saints_api/model/Miracle.java
+++ src/main/java/br/com/bth8/catholic_saints_api/model/Miracle.java
@@ -1,4 +1,37 @@
 package br.com.bth8.catholic_saints_api.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.util.UUID;
+
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "miracles")
 public class Miracle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(nullable = false, length = 70)
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Saint author;
+
+    @NotBlank
+    @Column(nullable = false, length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private  String location;
+
 }
--- src/main/java/br/com/bth8/catholic_saints_api/model/Saint.java
+++ src/main/java/br/com/bth8/catholic_saints_api/model/Saint.java
@@ -1,5 +1,52 @@
 package br.com.bth8.catholic_saints_api.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name = "saints")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)//Define que todos os dados das subclasses serão armazenados na tabela desta classe.
+@DiscriminatorColumn(//usada para definir uma coluna na tabela do banco de dados que será usada para distinguir
+        name = "SaintType",
+        discriminatorType = DiscriminatorType.STRING
+
+)
 public abstract class Saint {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @NotBlank
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(name = "baptism_date")
+    @Temporal(TemporalType.DATE)
+    private Date baptismDate;
+
+    @Column(name = "death_date")
+    @Temporal(TemporalType.DATE)
+    private Date deathDate;
+
+    @NotBlank
+    @Column(name = "canocization_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date canonizationDate;
+
+    @Column(nullable = false)
+    @OneToMany(mappedBy = "saint")
+    private List<Miracle> miracles;
+
 }
